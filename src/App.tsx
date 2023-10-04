84cb1abb2976194475006363a6713c0deba65e7c
muhamadzolfaghari <muhamad.zolfaghari@gmail.com>
2023-10-04 17:32:03 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Grid } from "@mui/material";
+
+const App = () => (
+  <Grid>
+    <Grid>test</Grid>
+    <Grid>test</Grid>
+  </Grid>
+);
+
+export default App;
